baab251e276adbaaf9372ff3ff3c95f42aa260e3
Tvé jméno <tvuj@email.cz>
2025-10-09 09:40:01 +0200

    ukazka mince sazka

--- 0. ukazky_kodu_it1/pole/vypis_pole.cpp
+++ 0. ukazky_kodu_it1/pole/vypis_pole.cpp
@@ -3,9 +3,10 @@
 int main() {
 	// vypis hodnot z pole
 	
+	int delka_pole = 10;
 	int pole[] = {42, 11, 2, 5, 6, 9, 14, 5, 89, 0};
 	
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < delka_pole; i++) {
 		printf("%d\n", pole[i]);
 	}
 	
